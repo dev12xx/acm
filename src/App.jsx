ea3a9b6b67a0e0f1d5bbb66168453af76d7fccf9
fares <faresberrahil@gmail.com>
2025-05-05 18:56:06 +0100

    ajouter du projer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 
-// import Titre from './component/Titre';
-// import Titre1 from './component/Titre1';
+import Titre from './component/Titre';
+import Titre1 from './component/Titre1';
 import Compteur from './component/compteur';
-// import Text from './component/Text';
-// import Ph from './component/Ph';   
-// import Formulaire from "./component/Formulaire";
-// import Calculatrice from "./component/Calculatrice";
+import Text from './component/Text';
+import Ph from './component/Ph';   
+import Formulaire from "./component/Formulaire";
+import Calculatrice from "./component/Calculatrice";
 //   import './App.css';
 import Message from "./component/Message";
 import Paragraphe from './component/paragraphe';
 import User from './component/user';
 
+
    function App() {
      return (
        <div>
-         {/* <Titre1/>
-         <Titre/> */}
+         <Titre1/>
+         <Titre/>
          <Compteur/>
-         {/* <Text/> */}
-         {/* <Ph/>    */}
-         {/* <Formulaire/>     */}
-         {/* <Calculatrice/> */}
+         <Text/>
+         <Ph/>   
+         <Formulaire/>    
+         <Calculatrice/>
         <Message/>         
         <Paragraphe/>
         <User/>
